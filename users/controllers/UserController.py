74119efba7ceeb10126c103b48eaa8736a697ca0
tafita37 <118960434+tafita37@users.noreply.github.com>
2025-08-05 16:34:09 +0300

    feat(sign_and_save_file)

--- users/controllers/UserController.py
+++ users/controllers/UserController.py
@@ -1,4 +1,7 @@
 # views.py
+import base64
+from datetime import datetime
+import hashlib
 from django.shortcuts import render, redirect
 from django.views.decorators.http import require_GET, require_POST
 from django.core.files.storage import FileSystemStorage
@@ -7,6 +10,10 @@ import json
 from web import settings 
 from django.utils.text import get_valid_filename
 from django.core.files.storage import default_storage
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
 
 @require_GET
 def register_user_page(request):
@@ -25,6 +32,22 @@ def load_upload_file_page(request):
         return redirect('login_user_page')
     else :
         return render(request, "views/upload_signature_file.html")
+    
+@require_GET
+def load_list_file_page(request):
+    if 'user_name' not in request.session:
+        return redirect('login_user_page')
+    
+    user_name = request.session['user_name']
+    uploads_path = os.path.join(settings.BASE_DIR, "signature_file.json")
+    try:
+        with open(uploads_path, 'r', encoding='utf-8') as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        data = {}
+    
+    user_files = data.get(user_name, [])
+    return render(request, "views/list_signature_file.html", {"user_files": user_files, "user_name": user_name, 'MEDIA_URL': settings.MEDIA_URL})
 
 @require_POST
 def login_user(request):
@@ -36,7 +59,7 @@ def login_user(request):
             if user_name not in registre :
                 raise Exception("L'utilisateur n'existe pas.")
     request.session['user_name'] = user_name
-    return redirect('upload_file_page')   
+    return redirect('list_file_page')   
 
 @require_POST
 def register_user(request):
@@ -62,7 +85,6 @@ def register_user(request):
         registre[user_name] = new_filename
         with open(registre_path, "w", encoding="utf-8") as f:
             json.dump(registre, f, ensure_ascii=False, indent=4)
-    print(user_name, public_key)
     return redirect('register_user_page')
 
 @require_POST
@@ -70,26 +92,119 @@ def upload_file_to_sign(request):
     file_to_sign = request.FILES.get('file_to_sign')
     user_name = request.session.get('user_name')
 
-    # Refuse si ce n’est pas un fichier .txt
-    if not file_to_sign.name.lower().endswith('.txt'):
-        raise Exception("Le fichier de clé publique doit être au format texte (.txt).")
+    if not user_name or not file_to_sign:
+        raise Exception("Utilisateur ou fichier non fourni.")
 
+    # Refuse les fichiers non .txt
+    if not file_to_sign.name.lower().endswith('.txt'):
+        raise Exception("Le fichier à signer doit être au format .txt.")
+
+    # Dossier utilisateur
     base_dir = os.path.join(settings.MEDIA_ROOT, 'uploads', user_name)
     os.makedirs(base_dir, exist_ok=True)
 
+    # Nom de fichier sécurisé
     original_name = get_valid_filename(file_to_sign.name)
     file_path = os.path.join(base_dir, original_name)
 
-    # Gestion des doublons : ajout de (1), (2), ...
+    # Gérer les doublons
     counter = 1
     file_root, file_ext = os.path.splitext(original_name)
     while os.path.exists(file_path):
         new_name = f"{file_root} ({counter}){file_ext}"
         file_path = os.path.join(base_dir, new_name)
         counter += 1
-    
+
+    final_filename = os.path.basename(file_path)
+
+    # Sauvegarde du fichier
     with default_storage.open(file_path, 'wb+') as destination:
         for chunk in file_to_sign.chunks():
             destination.write(chunk)
 
-    return redirect('upload_file_page')
+    # Enregistrement JSON
+    json_path = os.path.join(settings.BASE_DIR, "signature_file.json")
+    if os.path.exists(json_path):
+        with open(json_path, 'r', encoding='utf-8') as f:
+            signature_files = json.load(f)
+    else:
+        signature_files = {}
+
+    file_entry = {
+        "unsigned_file": final_filename,
+        "signed_file": ""
+    }
+
+    if user_name not in signature_files:
+        signature_files[user_name] = [file_entry]
+    else:
+        signature_files[user_name].append(file_entry)
+
+    with open(json_path, 'w', encoding='utf-8') as f:
+        json.dump(signature_files, f, ensure_ascii=False, indent=4)
+
+    return redirect('list_file_page')
+
+def sha256_hash(message: str) -> str:
+    # Convertir le message en bytes (UTF-8)
+    message_bytes = message.encode('utf-8')
+    
+    # Calcul du hash
+    sha256 = hashlib.sha256()
+    sha256.update(message_bytes)
+    
+    # Retourne le hash au format hexadécimal
+    return sha256.hexdigest()
+
+@require_GET
+def sign_file(request):
+    file_name = request.GET.get('file_name')
+    user_name = request.session.get('user_name')
+    
+    file_path = os.path.join(settings.MEDIA_ROOT, 'uploads', user_name, file_name)
+    with open(file_path, 'r', encoding='utf-8') as f:
+        content = f.read()
+    
+    digest = hashes.Hash(hashes.SHA256(), backend=default_backend())
+    digest.update(content.encode('utf-8'))  
+    hash_bytes = digest.finalize()
+    
+    private_key_path = os.path.join(settings.MEDIA_ROOT, f"{user_name}_private.pem")
+    with open(private_key_path, "rb") as key_file:
+        private_key = serialization.load_pem_private_key(
+            key_file.read(),
+            password=None,
+            backend=default_backend()
+        )
+    
+    signature = private_key.sign(
+        hash_bytes,
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+    
+    signature_b64 = base64.b64encode(signature).decode('utf-8')
+    
+    sig_file_name = file_name.replace('.txt', '.sig')
+    sig_file_path = os.path.join(settings.MEDIA_ROOT, 'uploads', user_name, sig_file_name)
+    with open(sig_file_path, 'wb') as sig_file:
+        sig_file.write(signature)
+        
+    json_path = os.path.join(settings.BASE_DIR, "signature_file.json")
+    with open(json_path, 'r', encoding='utf-8') as f:
+        signature_files = json.load(f)
+    signature_files[user_name]
+    
+    for i in range(0, len(signature_files[user_name])):
+        if signature_files[user_name][i].get("unsigned_file") == file_name:
+            signature_files[user_name][i]["signed_file"] = sig_file_name
+            signature_files[user_name][i]["signature"] = signature_b64
+            signature_files[user_name][i]["timestamp"] = timestamp = datetime.utcnow().isoformat() + "Z"
+            break
+        
+    with open(json_path, 'w', encoding='utf-8') as f:
+        json.dump(signature_files, f, ensure_ascii=False, indent=4)
+    return redirect('list_file_page')
--- users/urls/urls.py
+++ users/urls/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from users.controllers.UserController import load_upload_file_page, login_user, login_user_page, register_user_page, register_user, upload_file_to_sign
+from users.controllers.UserController import load_list_file_page, load_upload_file_page, login_user, login_user_page, register_user_page, register_user, sign_file, upload_file_to_sign
 
 urlpatterns = [
     path('register_page/', register_user_page, name='register_user_page'),
@@ -7,5 +7,7 @@ urlpatterns = [
     path('login_page/', login_user_page, name='login_user_page'),
     path('login_user/', login_user, name='login_user'),
     path('upload_file_page/', load_upload_file_page, name='upload_file_page'),
+    path('list_file_page/', load_list_file_page, name='list_file_page'),
     path('upload_file_to_sign/', upload_file_to_sign, name='upload_file_to_sign'),
+    path('sign_file/', sign_file, name='sign_file'),
 ]
